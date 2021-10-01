38db202acb49747e0b3cb3f20c35e7a689519bd8
Anastasia Grabovska <nastyusha.gr@gmail.com>
2021-10-01 21:34:24 +0300

    first commit

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>Hello everyone!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +18,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: 'gray',
+    fontSize: 40
+  }
 });
